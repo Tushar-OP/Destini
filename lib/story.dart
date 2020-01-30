2c4594928ec1d56a985b84f5b21fe406b1a78657
Tushar Patil <tusharpatilmarch@gmail.com>
2020-01-30 23:43:29 +0530

    Initial commit

---
+++ lib/story.dart
@@ -0,0 +1,11 @@
+class Story {
+  String storyTitle;
+  String choice1;
+  String choice2;
+
+  Story({this.storyTitle, this.choice1, this.choice2}) {
+//    _storyTitle = storyTitle;
+//    _choice1 = choice1;
+//    _choice2 = choice2;
+  }
+}
